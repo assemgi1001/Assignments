0867fcad85986dfeb5462da2de687d812f01900d
Assemgul <assemgi1001@gmail.com>
2021-05-31 18:20:15 -0700

    assigment2week-1

---
+++ Assign2.py
@@ -0,0 +1,27 @@
+
+highest_score = 0
+#open function
+# result_f = file handle
+result_f = open("Practice.txt")
+for line in result_f:
+    (name, score) = line.split()
+    # split function extracts the scores from the file
+    if float(score) > highest_score:
+        highest_score = float(score)
+result_f.close()
+print("The highest score was:")
+print(highest_score)
+
+scores = {}
+result_f = open("Practice.txt")
+for line in result_f:
+    (name, score) = line.split()
+    scores[score] = name
+result_f.close()
+print("The top score were:")
+for each_score in scores.keys():
+    # iterating over each of the rows using for loop
+    print('Surfer ' + scores[each_score] + ' scored ' + each_score)
+
+#persitent - stored in the Disk storage
+# array is collection of variables
